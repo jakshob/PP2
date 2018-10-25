597b0562a4a2e5ba667bfd564a87fd7bcb58cec3
Allan Carlsen <allantcarlsen@gmail.com>
2018-10-25 10:56:33 +0200

    Added Post

--- ConsoleApp1/ConsoleApp1/Post.cs
+++ ConsoleApp1/ConsoleApp1/Post.cs
@@ -6,5 +6,15 @@ namespace DomainModel
 {
     class Post
     {
+        public int Id { get; set; }
+        public int QA_UserId { get; set; }
+        public string Body { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Score { get; set; }
+        public int PostTypeId { get; set; }
+        public int ParrentId { get; set; }
+        public DateTime CloseDate { get; set; }
+        public string Title { get; set; }
+        public int LinkPostId { get; set; }
     }
 }
