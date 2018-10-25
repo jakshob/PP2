d146bb12837cb5783d61395d2b3978cfc85c6cb2
Allan Carlsen <allantcarlsen@gmail.com>
2018-10-25 12:23:06 +0200

    Added to IDataService

--- ConsoleApp1/ConsoleApp1/IDataService.cs
+++ ConsoleApp1/ConsoleApp1/IDataService.cs
@@ -7,6 +7,16 @@ namespace DomainModel
     interface IDataService
     {
         List<Comment> GetComments(int page, int pageSize);
-
+        Comment GetComment(int id);
+        List<Question> GetQuestions(int page, int pageSize);
+        List<Question> GetQuestionsBySearchSortedByScore(string searchText, int page, int pageSize);
+        List<Answer> GetAnswerToQuestion(int postId, int page, int pageSize);
+        Question GetQuestion(int id);
+        List<Question> GetFavorites(string sova_UserUsername, int page, int pageSize);
+        List<string> GetHistory(string sova_UserUsername, int page, int pageSize);
+        List<string> GetRelatedKeywords(string searchText);
+        List<string> GetMostUsedSearchTexts(int page, int pageSize);
+        Favorite CreateFavoriteQuestion(int questionId, string sova_UserUsername);
+        Favorite CreateFavoriteQuestion(int questionId, string sova_UserUsername, string note);
     }
 }
