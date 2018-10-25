fc749407eba087d310cd7e3d3bf6e4e3c0034020
Ndcs <Nikolaj@ndcs.dk>
2018-10-25 12:12:55 +0200

    Webservice&Dataservice_Fungerer_MedTestsOgAPI_#selvtak_:-)

---
+++ SovaProjectPart2/DataService/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }     
+        public int Posttype { get; set; }
+        //public string Body { get; set; }
+        public int? ParentId { get; set; }
+        public int Score { get; set; }
+    }
+}
