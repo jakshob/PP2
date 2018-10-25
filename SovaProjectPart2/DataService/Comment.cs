c5b8036f9d381bddb289e042741346e9ed3c4276
Ndcs <Nikolaj@ndcs.dk>
2018-10-25 13:55:30 +0200

    Comments_class_Included

---
+++ SovaProjectPart2/DataService/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel
+{
+    class Comment
+    {
+        public int Id { get; set; }
+        public int QA_UserId { get; set; }
+        public int PostId { get; set; }
+        public Answer Answer { get; set; }
+        public Question Question { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Score { get; set; }
+    }
+}
