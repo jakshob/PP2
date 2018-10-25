8b1d09fa00c4e6f3c7ab8f6416d06646944ce926
Allan Carlsen <allantcarlsen@gmail.com>
2018-10-25 11:34:32 +0200

    Added to Favorite

--- ConsoleApp1/ConsoleApp1/Favorite.cs
+++ ConsoleApp1/ConsoleApp1/Favorite.cs
@@ -6,5 +6,8 @@ namespace DomainModel
 {
     class Favorite
     {
+        public int PostId { get; set; }
+        public string SOVA_UserUsername { get; set; }
+        public string Note { get; set; }
     }
 }
