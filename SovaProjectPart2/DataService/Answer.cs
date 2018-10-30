8b4b8045214adbbeba442483733e5ea6a9d675a7
Jens Bo Pedersen <jensp25@gmail.com>
2018-10-30 10:51:47 +0100

    Body er blevet udkommenteret i Answer klassen

--- SovaProjectPart2/DataService/Answer.cs
+++ SovaProjectPart2/DataService/Answer.cs
@@ -8,7 +8,7 @@ namespace DomainModel
     {
         public int Id { get; set; }
         public int QA_UserId { get; set; }
-        public string Body { get; set; }
+        //public string Body { get; set; }
         public DateTime CreationDate { get; set; }
         public int Score { get; set; }
         public int ParentId { get; set; }
