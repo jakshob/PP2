b429196a2557e9819e62d9e7673967456c084e5a
Allan Carlsen <allantcarlsen@gmail.com>
2018-10-30 12:20:42 +0100

    Mapping tilbage til før

--- SovaProjectPart2/DataService/SovaContext.cs
+++ SovaProjectPart2/DataService/SovaContext.cs
@@ -5,54 +5,59 @@ namespace DomainModel
 {
     public class SovaContext : DbContext
     {
-       
-		public DbSet<Comment> Comments { get; set; }
-		public DbSet<Question> Questions { get; set; }
-		public DbSet<Answer> Answers { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<QA_User> Qa_Users { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<SOVA_User> SOVA_Users { get; set; }
 
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseNpgsql("host=localhost;db=stackoverflow;uid=postgres;pwd=Pluto25");
+            optionsBuilder.UseNpgsql("host=localhost;db=stackoverflow;uid=postgres;pwd=olivia");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-			//Comment
-			modelBuilder.Entity<Comment>().ToTable("comment");
-			modelBuilder.Entity<Comment>().Property(x => x.Id).HasColumnName("id");
-			modelBuilder.Entity<Comment>().Property(x => x.PostId).HasColumnName("postid");
-			modelBuilder.Entity<Comment>().Property(x => x.Score).HasColumnName("score");
-			modelBuilder.Entity<Comment>().Property(x => x.QA_UserId).HasColumnName("authorid");
-			modelBuilder.Entity<Comment>().Property(x => x.CreationDate).HasColumnName("creationdate");
-			modelBuilder.Entity<Comment>().Property(x => x.Text).HasColumnName("body");
+            //Fra Allan - Modellering:
 
-			//Answer
-			modelBuilder.Entity<Answer>().ToTable("answers");
-			modelBuilder.Entity<Answer>().Property(x => x.Id).HasColumnName("id");
-			//modelBuilder.Entity<Post>().Property(x => x.Body).HasColumnName("body");
-			modelBuilder.Entity<Answer>().Property(x => x.Score).HasColumnName("score");
-			modelBuilder.Entity<Answer>().Property(x => x.ParentId).HasColumnName("parentid");
-			modelBuilder.Entity<Answer>().Property(x => x.QA_UserId).HasColumnName("ownerid");
-			modelBuilder.Entity<Answer>().Property(x => x.CreationDate).HasColumnName("creationdate");
+            //Comment
+            modelBuilder.Entity<Comment>().ToTable("comment");
+            modelBuilder.Entity<Comment>().Property(x => x.Id).HasColumnName("id");
+            modelBuilder.Entity<Comment>().Property(x => x.PostId).HasColumnName("postid");
+            modelBuilder.Entity<Comment>().Property(x => x.Text).HasColumnName("body");
+            modelBuilder.Entity<Comment>().Property(x => x.Score).HasColumnName("score");
+            modelBuilder.Entity<Comment>().Property(x => x.CreationDate).HasColumnName("creationdate");
+            modelBuilder.Entity<Comment>().Property(x => x.QA_UserId).HasColumnName("authorid");
+            //DE FØLGENDE EKSISTERER JO IKKE I TABLES??
+            //modelBuilder.Entity<Comment>().Property(x => x.Answer).HasColumnName("answer");
+            //modelBuilder.Entity<Comment>().Property(x => x.Question).HasColumnName("question");
 
-			//Question
-			modelBuilder.Entity<Question>().ToTable("questions");
-			modelBuilder.Entity<Question>().Property(x => x.Id).HasColumnName("id");
-			modelBuilder.Entity<Question>().Property(x => x.Name).HasColumnName("title");
-			//modelBuilder.Entity<Post>().Property(x => x.Body).HasColumnName("body");
-			modelBuilder.Entity<Question>().Property(x => x.Score).HasColumnName("score");
-			modelBuilder.Entity<Question>().Property(x => x.QA_UserId).HasColumnName("ownerid");
-			modelBuilder.Entity<Question>().Property(x => x.CreationDate).HasColumnName("creationdate");
-			modelBuilder.Entity<Question>().Property(x => x.CloseDate).HasColumnName("closeddate");
+            //Favorites
+            modelBuilder.Entity<Favorite>().ToTable("favorites");
+            modelBuilder.Entity<Favorite>().Property(x => x.PostId).HasColumnName("postid");
+            modelBuilder.Entity<Favorite>().Property(x => x.SOVA_UserUsername).HasColumnName("username");
+            modelBuilder.Entity<Favorite>().Property(x => x.Note).HasColumnName("note");
 
-			modelBuilder.Entity<Question>().HasOne(p => p.Answer)
-				.WithOne(p => p.Question).HasForeignKey<Answer>(p => p.Id);
-			modelBuilder.Entity<Answer>().HasOne(p => p.Question)
-				.WithOne(p => p.Answer).HasForeignKey<Answer>(p => p.Id);
-		}
+            //History
+            modelBuilder.Entity<History>().ToTable("history");
+            modelBuilder.Entity<History>().Property(x => x.SOVA_UserUsername).HasColumnName("username");
+            modelBuilder.Entity<History>().Property(x => x.CreationDate).HasColumnName("creation_date");
+            modelBuilder.Entity<History>().Property(x => x.SearchText).HasColumnName("search_text");
+
+            //QA-user
+            modelBuilder.Entity<QA_User>().ToTable("qauser");
+            modelBuilder.Entity<QA_User>().Property(x => x.Id).HasColumnName("id");
+            modelBuilder.Entity<QA_User>().Property(x => x.DisplayName).HasColumnName("displayname");
+            modelBuilder.Entity<QA_User>().Property(x => x.CreationDate).HasColumnName("creationdate");
+            modelBuilder.Entity<QA_User>().Property(x => x.Location).HasColumnName("location");
+            modelBuilder.Entity<QA_User>().Property(x => x.Age).HasColumnName("age");
+
+        }
     }
 }
