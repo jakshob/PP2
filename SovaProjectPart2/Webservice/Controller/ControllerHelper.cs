04e65a29d8374ac9e8edeb11286cebbf317fe0f2
jakshob <jakshob@hotmail.com>
2018-10-30 15:15:31 +0100

    Helper controller

---
+++ SovaProjectPart2/Webservice/Controller/ControllerHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webservice.Controllers
+{
+    public class ControllerHelper : Controller
+    {
+        public int ComputeNumberOfPages(int pageSize, int numberOfItems)
+        {
+            return (int)Math.Ceiling((double)numberOfItems / pageSize);
+        }
+
+        public string CreateLink(string listMethod, int page, int pageSize)
+        {
+            return Url.Link(nameof(listMethod), new { page, pageSize });
+        }
+    }
+}
