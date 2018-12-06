e05c80a455ae5c0ab847e78b3150a1149901a3f9
Allan Carlsen <allantcarlsen@gmail.com>
2018-12-06 15:25:16 +0100

    Added Lib

--- SovaProjectPart2/Webservice/wwwroot/js/main.js
+++ SovaProjectPart2/Webservice/wwwroot/js/main.js
@@ -1,1 +1,34 @@
-﻿
+﻿require.config({
+    baseUrl: "js",
+    paths: {
+        knockout: "lib/knockout/dist/knockout.debug",
+        jquery: "lib/jQuery/dist/jquery",
+
+        text: 'lib/text/text',
+        dataService: 'services/dataService',
+        postman: 'services/postman'
+    },
+    shim: {
+        // set default deps
+        'jqcloud': ['jquery']
+    }
+});
+
+// register components
+require(['knockout'], function (ko) {
+    ko.components.register("post-list", {
+        viewModel: { require: 'components/postList/postList' },
+        template: { require: 'text!components/postList/postListView.html' }
+    });
+
+    ko.components.register("post", {
+        viewModel: { require: 'components/post/post' },
+        template: { require: 'text!components/post/postView.html' }
+    });
+
+
+});
+
+require(['knockout', 'app'], function (ko, app) {
+    ko.applyBindings(app);
+});
